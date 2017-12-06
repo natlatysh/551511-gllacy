15345e955ec37c4ec7dd22d7bec342f434ac56c4
Наталья Латышева <32721007+natlatysh@users.noreply.github.com>
2017-12-06 22:05:02 +0300

    доработка фона модального окна

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 var btn = document.querySelector(".btn-feedback");
 var modal = document.querySelector(".feedback-form");
 var close = modal.querySelector(".delete");
-var overlay = document.querySelector(".overlay");
+var wrapper = document.querySelector(".feedback-wrapper");
 
 btn.addEventListener("click", function (evt) {
-modal.classList.add("modal-show");
-overlay.classList.add("modal-show");
+wrapper.classList.add("modal-show");
 });
 
 close.addEventListener("click", function (evt) {
 evt.preventDefault();
-modal.classList.remove("modal-show");
-overlay.classList.remove("modal-show");
+wrapper.classList.remove("modal-show");
 });
 
 window.addEventListener("keydown", function (evt) {
 if (evt.keyCode === 27) {
-if (modal.classList.contains("modal-show")) {
-  modal.classList.remove("modal-show");
-  overlay.classList.remove("modal-show");
+if (wrapper.classList.contains("modal-show"))  {
+  wrapper.classList.remove("modal-show");
 }
 }
 });
+
+wrapper.addEventListener("click", function (evt) {
+evt.preventDefault();
+wrapper.classList.remove("modal-show");
+  });
