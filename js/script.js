4a95f730edb2f469dc172fb760225368bce02058
Наталья Латышева <32721007+natlatysh@users.noreply.github.com>
2017-12-08 23:20:42 +0300

    доработка

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
 var btn = document.querySelector(".btn-feedback");
 var wrapper = document.querySelector(".feedback-wrapper");
 var close = wrapper.querySelector(".delete");
-var name = document.querySelector(".name");
-
-btn.addEventListener("click", function (evt) {
-wrapper.classList.add("modal-show");
-name.focus();
+var login = wrapper.querySelector(".name");
+btn.addEventListener("click", function(evt) {
+    wrapper.classList.add("modal-show");
+    login.focus();
 });
-
-close.addEventListener("click", function (evt) {
-wrapper.classList.remove("modal-show");
+close.addEventListener("click", function(evt) {
+    wrapper.classList.remove("modal-show");
 });
-
-window.addEventListener("keydown", function (evt) {
-if (evt.keyCode === 27) {
-if (wrapper.classList.contains("modal-show"))  {
-  wrapper.classList.remove("modal-show");
-}
-}
+window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        if (wrapper.classList.contains("modal-show")) {
+            wrapper.classList.remove("modal-show");
+        }
+    }
 });
