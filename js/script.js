a03e73f3e8259b230502b955c5c04c6b7d258a4f
Наталья Латышева <32721007+natlatysh@users.noreply.github.com>
2017-12-12 15:13:15 +0300

    доработка финальной сдачи

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 var btn = document.querySelector(".btn-feedback");
 var wrapper = document.querySelector(".feedback-wrapper");
 var close = wrapper.querySelector(".delete");
-var login = wrapper.querySelector(".name");
+var name = wrapper.querySelector(".name");
+var form = wrapper.querySelector("form");
+var email = wrapper.querySelector(".email");
+var textarea = wrapper.querySelector(".textarea");
 btn.addEventListener("click", function(evt) {
     wrapper.classList.add("modal-show");
-    login.focus();
+    name.focus();
 });
 close.addEventListener("click", function(evt) {
     wrapper.classList.remove("modal-show");
+    wrapper.classList.remove("modal-error");
 });
 window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
         if (wrapper.classList.contains("modal-show")) {
             wrapper.classList.remove("modal-show");
+            wrapper.classList.remove("modal-error");
         }
     }
 });
+form.addEventListener("submit", function (evt) {
+    if (!textarea.value || !email.value) {
+        evt.preventDefault();
+        wrapper.classList.remove("modal-error");
+        wrapper.offsetWidth = wrapper.offsetWidth;
+        wrapper.classList.add("modal-error");
+}   else {
+        localStorage.setItem("email", email.value);
+}
+  });
